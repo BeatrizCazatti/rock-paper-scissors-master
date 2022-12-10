7baddf23f3ad15acfee878fd05c039bf972f1b3c
Beatriz <beatrizcazatti@gmail.com>
2022-12-10 11:52:58 -0300

    Adiciona animação para a opção do computador(problema: aparece antes da animação começar)

--- src/script.js
+++ src/script.js
@@ -16,8 +16,6 @@ function hideElement (){
     blurPlan.classList.remove('show')
 }
 
-
-
 const scissorsElement = document.getElementById('scissors')
 const paperElement = document.getElementById('paper')
 const rockElement = document.getElementById('rock')
@@ -33,36 +31,55 @@ lizardElement.addEventListener('click', selectOption, true)
 spocksElement.addEventListener('click', selectOption, true)
 
 
+
+//Evento ao clicar na opção
 function selectOption (optionSelect) {
     const elementsHTML = [scissorsElement, paperElement, rockElement, lizardElement, spocksElement]
-    var options = ['scissors', 'paper', 'rock', 'lizard', 'spocks']
+    const options = ['scissors', 'paper', 'rock', 'lizard', 'spocks']
 
+    //Identifica qual elemento foi clicado
     options.map(function(elem, index){
         if(elem == optionSelect.srcElement.id){
             elementsHTML[index].style.animation=`diagonal-move-anim-${optionSelect.srcElement.id} .5s linear 1 forwards`
             elementsHTML.splice(index, 1)
-            console.log(index)
         }
     })
+    
+    //Salva na variável a opção  do usuário
+    var index = options.indexOf(optionSelect.srcElement.id)
+    console.log(index)
+    
+    //Define opção do computador(nº aleatório)
+    var randonNumber = Math.floor(Math.random() * 5)
+    console.log('index:'+index)
+    console.log('num:'+randonNumber)
+    if (index == randonNumber) {
+        while (index == randonNumber){
+            randonNumber = Math.floor(Math.random() * 5)
+            console.log('segundo' + randonNumber)
+        }
+    }
+    
+    //Mostra a opção do pc
     pentagonElement.style.opacity = '0'
     
     elementsHTML.map(function(elem) {
-        elem.style.opacity = '0'
+        elem.classList.add('hide')
     })
-
+    
     const titlePlayer = document.querySelector(`#${optionSelect.srcElement.id}>.title-player`)
     const titleHouse = document.getElementsByClassName('title-house')[0]
 
     const circleOption = document.getElementById('circle-picked')
-    titlePlayer.style.transitionDelay = ".5s"
     titlePlayer.style.color = "#e3e3e3"
     titleHouse.style.color = "#e3e3e3"
     circleOption.style.opacity = '1'
-    circleOption.style.display = 'initial'
-    circleOption.style.transitionDelay = '3s'
+        
+    const optionComputer = randonNumber
+    showSpecial(elementsHTML, optionComputer)
+}
 
-    const optionComputer = Math.floor(Math.random() * 5)
-    elementsHTML[optionComputer].classList.add('option-computer-select')
-    elementsHTML[optionComputer].style.transitionDelay = "3s"
-    
+function showSpecial (elemnts, option){
+    elemnts[option].classList.add('option-computer-select')
+    elemnts[option].classList.add('border')
 }
